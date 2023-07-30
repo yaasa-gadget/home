22f581a59440afee33d83994fbbd2a3ffbf00eee
yaasa-linux <113877487+yaasa-linux@users.noreply.github.com>
2023-07-30 16:45:35 +0900

    Delete sidemenu.js

--- sidemenu.js
+++
@@ -1,9 +0,0 @@
-$(".openbtn").click(function () {
-	$(this).toggleClass('active');
-    $("#sidemenu").toggleClass('panelactive');
-});
-
-$("#sidemenu a").click(function () {
-    $(".openbtn").removeClass('active');
-    $("#sidemenu").removeClass('panelactive');
-});
